30b2a059c194bc2101a86ccdb0bb7870f426471f
Никита Гребнев <nikitagrebnev@MacBook-Air-Nikita.local>
2024-11-08 20:26:32 +0600

    lections

--- sulpak_parse/main.py
+++ sulpak_parse/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import requests
 import json
-
+# from multiprocessing import Pool
 
 URL = 'https://www.sulpak.kg/f/'
 
@@ -26,7 +26,7 @@ def parse_cards(cards: list) -> list:
                 'title': card.get('data-name'),
                 'price': card.get('data-price'),
                 'brand': card.get('data-brand'),
-                'in_stock': card.find('div', class_='product__item-showcase').text if card.find('div', class_='product__item-showcase').text else 'Нет в наличии',
+                'in_stock': card.find('div', class_='product__item-showcase').text if card.find('div', class_='product__item-showcase') else 'Нет в наличии',
                 'product_link': 'https://www.sulpak.kg' + card.find('div', class_='product__item-name').find('a').get('href')
             })
         return data
@@ -40,3 +40,23 @@ def get_last_page(category: str) -> int:
     soup = BS(html, 'lxml')
     last_page = soup.find('div', class_='pagination').get('data-pagescount')
     return int(last_page)
+
+
+def save_to_json(data: list, filename: str) -> None:
+    with open(filename, 'w') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+
+def main(category: str) -> None:
+    data = []
+    last_page = get_last_page(category)
+    
+    for page in range(1, last_page + 1):
+        html = get_html(category, page)
+        cards = get_cards(html)
+        data.extend(parse_cards(cards))
+    save_to_json(data, f'{category}.json')
+
+
+main('holodilniki')
